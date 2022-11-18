0a6281e5ee7e90a178d4a978130c9f6b52d623c6
AndikaRB <111847058+AndikaRB@users.noreply.github.com>
2022-11-18 15:38:12 +0700

    G3 13 (#1)
    * Migrate database & setup FE (#3)
    
    * create database user
    
    user database with ORM
    
    * setup FE
    
    install library FE
    
    * Login Page
    
    Login Sementara
    
    Co-authored-by: Muhammad Fikri Fernanda Yusuf <68997783+mfikrif11@users.noreply.github.com>

---
+++ projects/client/src/redux/features/authSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    id: 0,
+    username: "",
+    email: "",
+    phone_number: 0,
+    profile_picture: "",
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.id = action.payload.id
+            state.username = action.payload.username
+            state.email = action.payload.email
+            state.phone_number = action.payload.phone_number
+            state.profile_picture = action.payload.profile_picture
+        },
+        logout: (state) => {
+            state.id = 0
+            state.username = ""
+            state.email = ""
+            state.phone_number = 0
+            state.profile_picture = ""
+        }
+    }
+})
+
+export const { login, logout } = authSlice.actions
+
+export default authSlice.reducer
