7f06398a451aac46e72e85f6f285b27b38724a6c
AndikaRB <111847058+AndikaRB@users.noreply.github.com>
2022-11-21 17:01:09 +0700

    G3-10 HomePage (#7)
    * G3-10 HomePage
    
    Home Page Sementara
    
    * G3-10 Fix
    
    change component folder to components

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -1,99 +1,117 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import LoginPage from "./pages/Login"
 import { useDispatch } from "react-redux"
 import { axiosInstance } from "./api"
 import { login } from "./redux/features/authSlice"
-import GuestRoute from "./component/GuestRoute"
+import GuestRoute from "./components/GuestRoute"
 import Register from "./pages/Register"
 import RegisterVerification from "./pages/RegisterVerification"
+import { Box } from "@chakra-ui/react"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import HomePage from "./pages/Home"
 import AdminDashboard from "./components/admin/AdminDashboard"
 import "./AdminDashboard.css"
 import SideNavBar from "./components/SideNavBar"
-import { Box } from "@chakra-ui/react"
 import WarehouseManagement from "./components/admin/WarehouseManagement"
 import ChangePassword from "./pages/profile/ChangePassword"
 import Profile from "./pages/profile/Profile"
 
 function App() {
-    const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("")
 
-    useEffect(() => {
-        ;(async () => {
-            const { data } = await axios.get(
-                `${process.env.REACT_APP_API_BASE_URL}/greetings`
-            )
-            setMessage(data?.message || "")
-        })()
-    }, [])
+  useEffect(() => {
+    ; (async () => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/greetings`
+      )
+      setMessage(data?.message || "")
+    })()
+  }, [])
 
-    const [authCheck, setAuthCheck] = useState(false)
+  const [authCheck, setAuthCheck] = useState(false)
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const keepUserLoggedIn = async () => {
-        try {
-            const auth_token = localStorage.getItem("auth_token")
+  const location = useLocation()
 
-            if (!auth_token) {
-                setAuthCheck(true)
-                return
-            }
+  const keepUserLoggedIn = async () => {
+    try {
+      const auth_token = localStorage.getItem("auth_token")
 
-            const response = await axiosInstance.get("/auth/refresh-token")
+      if (!auth_token) {
+        setAuthCheck(true)
+        return
+      }
 
-            dispatch(login(response.data.data))
+      const response = await axiosInstance.get("/auth/refresh-token")
 
-            localStorage.setItem("auth_token", response.data.token)
-            setAuthCheck(true)
-        } catch (err) {
-            console.log(err)
-            setAuthCheck(true)
-        } finally {
-            setAuthCheck(true)
-        }
+      dispatch(login(response.data.data))
+
+      localStorage.setItem("auth_token", response.data.token)
+      setAuthCheck(true)
+    } catch (err) {
+      console.log(err)
+      setAuthCheck(true)
+    } finally {
+      setAuthCheck(true)
     }
+  }
 
-    useEffect(() => {
-        keepUserLoggedIn()
-    }, [])
+  useEffect(() => {
+    keepUserLoggedIn()
+  }, [])
 
-    return (
-        <>
-            <SideNavBar />
-            <Box marginLeft="275px" marginTop={"50px"}>
-                <Routes>
-                    <Route
-                        path="/login"
-                        element={
-                            <GuestRoute>
-                                <LoginPage />
-                            </GuestRoute>
-                        }
-                    />
-                    <Route path="/register" element={<Register />} />
-                    <Route
-                        path="/register/verification"
-                        element={<RegisterVerification />}
-                    />
-                    <Route
-                        path="/admin-dashboard"
-                        element={<AdminDashboard />}
-                    />
-                    <Route
-                        path="/warehouse-management"
-                        element={<WarehouseManagement />}
-                    />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route
-                        path="/profile/change-password"
-                        element={<ChangePassword />}
-                    />
-                </Routes>
-            </Box>
-        </>
-    )
+  return (
+    <>
+      {location.pathname === "/login" || location.pathname === "/register" ? null : (
+        <Box>
+          <Navbar />
+        </Box>
+      )}
+
+      <SideNavBar />
+      <Box marginLeft="275px" marginTop={"50px"}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/login"
+            element={
+              <GuestRoute>
+                <LoginPage />
+              </GuestRoute>
+            }
+          />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/register/verification"
+            element={<RegisterVerification />}
+          />
+          <Route
+            path="/admin-dashboard"
+            element={<AdminDashboard />}
+          />
+          <Route
+            path="/warehouse-management"
+            element={<WarehouseManagement />}
+          />
+          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/profile/change-password"
+            element={<ChangePassword />}
+          />
+        </Routes>
+
+        {location.pathname === "/login" || location.pathname === "/register" ? null : (
+          <Box>
+            <Footer />
+          </Box>
+        )}
+      </Box>
+    </>
+  )
 }
 
 export default App
