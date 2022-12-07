8c131842728cbab3a70a976ca9a1b59999ec28b1
Joel Legifani Manisali <joellegifanimanisali@gmail.com>
2022-12-07 12:02:08 +0700

    G3 34 (#48)
    * G3-34
    
    Beress
    
    * Fix Route

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -1,144 +1,145 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
-import LoginPage from "./pages/Login";
-import { useDispatch, useSelector } from "react-redux";
-import { axiosInstance } from "./api";
-import { login } from "./redux/features/authSlice";
-import GuestRoute from "./components/GuestRoute";
-import Register from "./pages/Register";
-import RegisterVerification from "./pages/RegisterVerification";
-import { Box } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer/Footer";
-import HomePage from "./pages/Home";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import "./AdminDashboard.css";
-import SideNavBar from "./components/SideNavBar";
-import WarehouseManagement from "./pages/admin/AdminWarehouseManagement";
-import ChangePassword from "./pages/profile/ChangePassword";
-import Profile from "./pages/profile/Profile";
-import AdminRoute from "./components/admin/AdminRoute";
-import AddressList from "./pages/profile/AddressList";
-import { attach } from "./redux/features/resetSlice";
-import ResetPasswordConfirmation from "./pages/ResetPasswordConfirmation";
-import RequestResetPassword from "./pages/RequestResetPassword";
-import ManageUserData from "./components/admin/ManageUserData";
-import ManageAdminData from "./components/admin/ManageAdminData";
-import AdminCategory from "./pages/AdminCategory";
-import NotFound from "./components/404Page";
-import Cart from "./pages/Cart";
-import ProtectedRoute from "./components/ProtectedRoute";
-import ProductData from "./pages/admin/ProductData";
-import ProductDataDetail from "./pages/admin/ProductDataDetail";
-import Shipment from "./pages/shipment/Shipment";
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Route, Routes, useLocation } from "react-router-dom"
+import LoginPage from "./pages/Login"
+import { useDispatch, useSelector } from "react-redux"
+import { axiosInstance } from "./api"
+import { login } from "./redux/features/authSlice"
+import GuestRoute from "./components/GuestRoute"
+import Register from "./pages/Register"
+import RegisterVerification from "./pages/RegisterVerification"
+import { Box } from "@chakra-ui/react"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer/Footer"
+import HomePage from "./pages/Home"
+import AdminDashboard from "./pages/admin/AdminDashboard"
+import "./AdminDashboard.css"
+import SideNavBar from "./components/SideNavBar"
+import WarehouseManagement from "./pages/admin/AdminWarehouseManagement"
+import ChangePassword from "./pages/profile/ChangePassword"
+import Profile from "./pages/profile/Profile"
+import AdminRoute from "./components/admin/AdminRoute"
+import AddressList from "./pages/profile/AddressList"
+import Product from "./pages/product/Product"
+import ProductDetail from "./pages/product/ProductDetail"
+import { attach } from "./redux/features/resetSlice"
+import ResetPasswordConfirmation from "./pages/ResetPasswordConfirmation"
+import RequestResetPassword from "./pages/RequestResetPassword"
+import ManageUserData from "./components/admin/ManageUserData"
+import ManageAdminData from "./components/admin/ManageAdminData"
+import AdminCategory from "./pages/AdminCategory"
+import NotFound from "./components/404Page"
+import Cart from "./pages/Cart"
+import ProtectedRoute from "./components/ProtectedRoute"
+import ProductData from "./pages/admin/ProductData"
+import ProductDataDetail from "./pages/admin/ProductDataDetail"
+import Shipment from "./pages/shipment/Shipment"
 import UpdateStock from "./pages/admin/UpdateStock"
 import WarehouseStock from "./components/admin/WarehouseStock"
 
-
 function App() {
-  const [message, setMessage] = useState("");
-  const authSelector = useSelector((state) => state.auth);
+  const [message, setMessage] = useState("")
+  const authSelector = useSelector((state) => state.auth)
 
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       const { data } = await axios.get(
         `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
-  }, []);
+      )
+      setMessage(data?.message || "")
+    })()
+  }, [])
 
-  const [authCheck, setAuthCheck] = useState(false);
+  const [authCheck, setAuthCheck] = useState(false)
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const location = useLocation();
+  const location = useLocation()
 
   const keepUserLoggedIn = async () => {
     try {
-      const auth_token = localStorage.getItem("auth_token");
+      const auth_token = localStorage.getItem("auth_token")
 
       if (!auth_token) {
-        setAuthCheck(true);
-        return;
+        setAuthCheck(true)
+        return
       }
 
-      const response = await axiosInstance.get("/auth/refresh-token");
+      const response = await axiosInstance.get("/auth/refresh-token")
 
-      dispatch(login(response.data.data));
+      dispatch(login(response.data.data))
 
-      localStorage.setItem("auth_token", response.data.token);
-      setAuthCheck(true);
+      localStorage.setItem("auth_token", response.data.token)
+      setAuthCheck(true)
     } catch (err) {
-      console.log(err);
-      setAuthCheck(true);
+      console.log(err)
+      setAuthCheck(true)
     } finally {
-      setAuthCheck(true);
+      setAuthCheck(true)
     }
-  };
+  }
 
   const userResetData = async () => {
     try {
-      const reset_token = localStorage.getItem("reset_token");
+      const reset_token = localStorage.getItem("reset_token")
 
       if (!reset_token) {
-        setAuthCheck(true);
-        return;
+        setAuthCheck(true)
+        return
       }
 
-      const response = await axiosInstance.get("/auth/refresh-token");
+      const response = await axiosInstance.get("/auth/refresh-token")
 
-      dispatch(attach(response.data.data));
+      dispatch(attach(response.data.data))
 
-      localStorage.setItem("reset_token", response.data.token);
-      setAuthCheck(true);
+      localStorage.setItem("reset_token", response.data.token)
+      setAuthCheck(true)
     } catch (err) {
-      console.log(err);
-      setAuthCheck(true);
+      console.log(err)
+      setAuthCheck(true)
     } finally {
-      setAuthCheck(true);
+      setAuthCheck(true)
     }
-  };
+  }
 
   useEffect(() => {
-    keepUserLoggedIn();
-    userResetData();
-  }, []);
+    keepUserLoggedIn()
+    userResetData()
+  }, [])
 
-    return (
-        <>
-            {authSelector.RoleId === 3 || authSelector.RoleId === 2 ? (
-                <SideNavBar />
-            ) : null}
+  return (
+    <>
+      {authSelector.RoleId === 3 || authSelector.RoleId === 2 ? (
+        <SideNavBar />
+      ) : null}
 
-            {location.pathname === "/login" ||
-            location.pathname === "/register" ||
-            location.pathname === "/reset-password-confirmation" ||
-            location.pathname === "/request-reset-password" ||
-            authSelector.RoleId === 3 ||
-            authSelector.RoleId === 2 ? null : (
-                <Box>
-                    <Navbar />
-                </Box>
-            )}
+      {location.pathname === "/login" ||
+      location.pathname === "/register" ||
+      location.pathname === "/reset-password-confirmation" ||
+      location.pathname === "/request-reset-password" ||
+      authSelector.RoleId === 3 ||
+      authSelector.RoleId === 2 ? null : (
+        <Box>
+          <Navbar />
+        </Box>
+      )}
 
-            <Routes>
-                <Route path="/*" element={<NotFound />} />
-                <Route path="/" element={<HomePage />} />
-                <Route
-                    path="/login"
-                    element={
-                        <GuestRoute>
-                            <LoginPage />
-                        </GuestRoute>
-                    }
-                />
-                <Route
-                    path="/reset-password-confirmation"
-                    element={<ResetPasswordConfirmation />}
-                />
-
+      <Routes>
+        <Route path="/*" element={<NotFound />} />
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/login"
+          element={
+            <GuestRoute>
+              <LoginPage />
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/reset-password-confirmation"
+          element={<ResetPasswordConfirmation />}
+        />
+        
                 <Route
                     path="/admin/manage-admin-data"
                     element={
@@ -175,29 +176,29 @@ function App() {
                       </AdminRoute>
                     }
                   />
+                  
+        <Route
+          path="/request-reset-password"
+          element={
+            <GuestRoute>
+              <RequestResetPassword />
+            </GuestRoute>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/register/verification"
+          element={<RegisterVerification />}
+        />
 
-                <Route
-                    path="/request-reset-password"
-                    element={
-                        <GuestRoute>
-                            <RequestResetPassword />
-                        </GuestRoute>
-                    }
-                />
-                <Route path="/register" element={<Register />} />
-                <Route
-                    path="/register/verification"
-                    element={<RegisterVerification />}
-                />
-
-                <Route
-                    path="/cart"
-                    element={
-                        <ProtectedRoute>
-                            <Cart />
-                        </ProtectedRoute>
-                    }
-                />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
 
         <Route
           path="/admin/dashboard"
@@ -208,14 +209,14 @@ function App() {
           }
         />
 
-                <Route
-                    path="/admin/category"
-                    element={
-                        <AdminRoute>
-                            <AdminCategory />
-                        </AdminRoute>
-                    }
-                />
+        <Route
+          path="/admin/category"
+          element={
+            <AdminRoute>
+              <AdminCategory />
+            </AdminRoute>
+          }
+        />
 
         <Route
           path="/admin/warehouse-management"
@@ -275,6 +276,11 @@ function App() {
             </AdminRoute>
           }
         />
+        <Route path="/product-data" element={<ProductData />} />
+        <Route path="/product/detail/:id" element={<ProductDataDetail />} />
+        {/* Product Route */}
+        <Route path="/product" element={<Product />} />
+        <Route path="/product/:id/:product_name" element={<ProductDetail />} />
       </Routes>
 
       {location.pathname === "/login" ||
@@ -291,4 +297,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
