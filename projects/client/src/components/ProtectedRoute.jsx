95a30749324fe2828972f4a94ac8c40a335d1ace
AndikaRB <111847058+AndikaRB@users.noreply.github.com>
2022-12-25 19:32:22 +0700

    G3-15 Protected Route (#75)
    Fix Protected Route

--- projects/client/src/components/ProtectedRoute.jsx
+++ projects/client/src/components/ProtectedRoute.jsx
@@ -27,7 +27,7 @@ const ProtectedRoute = ({ children }) => {
 
     if (authSelector.id) {
         return (children)
-    } else if (!authSelector.id)
+    } else if (!authSelector.id) {
         return (
             <>
                 <AlertDialog isCentered closeOnOverlayClick={false} isOpen={isOpen} onClose={backToLogin} size={"sm"}
@@ -77,6 +77,7 @@ const ProtectedRoute = ({ children }) => {
                 </AlertDialog>
             </>
         )
+    }
 }
 
 export default ProtectedRoute
